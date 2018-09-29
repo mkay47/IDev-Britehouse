787b4f169215a6c270ccc903ec64e55b9f6a0bd0
Mkhululi Mtshemla <Mkhululi.Mtshemla@psg.co.za>
2018-09-29 10:57:00 +0200

    classes half done

---
+++ IDev-Solution/IDev-Solution/Controllers/DataCleanupController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackathon.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[Controller]")]
+    public class DataCleanupController : Controller
+    {
+        [Route("datacleanup")]
+        [HttpGet()]
+        public IActionResult DataCleanUp()
+        {
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ IDev-Solution/IDev-Solution/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackathon.Models
+{
+    public class Order
+    {
+        [JsonProperty("order_id")]
+        public string OrderID { get; set; }
+
+        [JsonProperty("priority")]
+        public string Priority { get; set; }
+
+        [JsonProperty("price")]
+        public string Price { get; set; }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+    }
+}
